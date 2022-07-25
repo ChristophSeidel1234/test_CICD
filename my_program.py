2eae862ac0ef9f511a8a98765d179c058359e347
Christoph Seidel <c.seidel1234@gmail.com>
2022-07-25 14:56:43 +0200

    my first commit with GitHub action

---
+++ my_program.py
@@ -0,0 +1,4 @@
+""" this is adocstring for the module """
+def square_number(number):
+    """ this is aq doctring for the function """
+    return number**2
---
+++ program_test.py
@@ -0,0 +1,6 @@
+"""this is a test suite for my_program.py"""
+import my_program
+
+def test_output():
+    """ this is a doctring """
+    assert my_program.square_number(2) == 4
